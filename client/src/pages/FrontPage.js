0d91abd980245f83e93950fcd249bb71a78ad721
jbpkp07 <jeremy.w.barnes@gmail.com>
2019-11-24 12:39:45 -0800

    Updated react-reveal animation for front page

--- client/src/pages/FrontPage.js
+++ client/src/pages/FrontPage.js
@@ -1,28 +1,45 @@
 // dependencies
 import React, { Component } from "react"
+import Flip from 'react-reveal/Flip';
 import { Col, Row, Container } from "../components/Grid/Grid"
 import Image from "../components/Image/Image"
 import logoImg from "../images/lrmLogo.png"
 
 class FrontPage extends Component {
 
+    constructor(props) {
+
+        super(props);
+        
+        this.state = { show: true };
+    }
+
     handleThisClick = () => {
-        this.props.history.push("/quiz")
+
+        this.setState({ show: false }, () => {
+           
+            setTimeout(() => {
+            
+                this.props.history.push("/quiz");     
+
+            }, 1000);
+        });
     }
 
     render() {
+
         return (
+
             <div>
                 <Container fluid>
                     <Row fluid>
                         <Col size="mx-auto">
                             <div onClick={this.handleThisClick}>
-                                <Image
-                                    image={logoImg}
-                                    name={logoImg.name}
-                                >
-                                    <h1>Which Political Candidate Are You Most Like?</h1>
-                                </Image>
+                                <Flip left when={this.state.show}>
+                                    <Image image={logoImg} name={logoImg.name} >
+                                        <h1>Which Political Candidate Are You Most Like?</h1>
+                                    </Image>
+                                </Flip>
                             </div>
                         </Col>
                     </Row>
